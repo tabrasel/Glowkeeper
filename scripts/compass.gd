8b926367a3ca687db4deca09c9978984989dc85f
Tate Brasel <tatebrasel@gmail.com>
2024-10-19 20:40:52 -0700

    Fix lantern compass off-by-1-pixel issue

--- scripts/compass.gd
+++ scripts/compass.gd
@@ -53,13 +53,14 @@ func _process(delta):
 	queue_redraw()
 
 func _draw():
-	var center: Vector2 = Vector2.ZERO
-	var north: Vector2 = Vector2(cos(_angle) * 3, sin(_angle) * 3)
-	var south: Vector2 = Vector2(-north.x, -north.y)
+	var center: Vector2i = Vector2.ZERO
+	var north: Vector2i = Vector2i(int(cos(_angle) * 7), int(sin(_angle) * 7))
+	var south: Vector2 = Vector2i(int(-north.x), int(-north.y))
 
 	var north_color = Color(1, 0, 0) if _state == CompassState.POINTING else Color(1, 1, 1) 
-	draw_line(center, north, north_color)
-	draw_line(center, south, Color(1, 1, 1))
+
+	_draw_segment(north, north_color)
+	_draw_segment(south, Color.WHITE)
 
 func _on_fireflies_deposited():
 	_angle_velocity = 0.3 * _next_spin_dir
@@ -69,4 +70,55 @@ func _on_fireflies_deposited():
 
 func _on_spin_timer_timeout():
 	choose_target()
+	
+	
+func _draw_segment(target: Vector2i, line_color: Color):
+	if target.x == 0 || target.y == 0:
+		# draw a single straight line
+		draw_line(Vector2.ZERO, target, line_color)
+	elif abs(target.x) > abs(target.y):
+		_draw_horizontal_segments(target, line_color)
+	else:
+		_draw_vertical_segments(target, line_color)
+	
 
+func _draw_horizontal_segments(target: Vector2, line_color: Color):
+	# should be negative ONLY if target.x is negative
+	var width = abs(target.x / target.y) * sign(target.x)
+
+	var float_x := 0.0
+
+	var sign_y = sign(target.y)
+	for int_y in abs(target.y):
+		# make y negative if required
+		var y: int = int_y * sign_y
+		# only convert to int when drawing
+		var ax := int(float_x)
+		var bx := int(float_x + width)
+
+		var a := Vector2(ax, y)
+		var b := Vector2(bx, y)
+		draw_line(a, b, line_color)
+
+		float_x += width
+
+
+func _draw_vertical_segments(target: Vector2, line_color: Color):
+	# should be negative ONLY if target.y is negative
+	var height = abs(target.y / target.x) * sign(target.y)
+
+	var float_y := 0.0
+
+	var sign_x = sign(target.x)
+	for int_x in abs(target.x):
+		# make x negative if required
+		var x: int = int_x * sign_x
+		# only convert to int when drawing
+		var ay := int(float_y)
+		var by := int(float_y + height)
+
+		var a := Vector2(x, ay)
+		var b := Vector2(x, by)
+		draw_line(a, b, line_color)
+
+		float_y += height
